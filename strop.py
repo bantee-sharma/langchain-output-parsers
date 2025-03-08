bca9b50318f7366379b0d9669dd0959050f3f3f1
bantee-sharma <banteesharma9@gmail.com>
2025-03-08 22:26:15 +0530

    Create strop.py

---
+++ strop.py
@@ -0,0 +1,29 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+template1 = PromptTemplate(
+    template="write a detail report on {topic}",
+    input_variables=["topic"]
+)
+
+
+
+template2 = PromptTemplate(
+    template="write a 5 line summary on following text. /n {text}",
+    input_variables=["text"]
+)
+
+prompt1 = template1.invoke({"topic":"Black Hole"})
+
+res = model.invoke(prompt1)
+
+prompt2 = template2.invoke({"text":res.content})
+
+res1 = model.invoke(prompt2)
+
+print(res1.content)
